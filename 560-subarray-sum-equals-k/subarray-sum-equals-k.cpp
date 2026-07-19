34cddba4dba5a755cda344dca21c1bbb83e956d6
Nirav <nirav9459@gmail.com>
2026-07-19 21:29:49 +0530

    Time: 71 ms (29.18%) | Memory: 47.6 MB (29.07%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,11 +4,9 @@ public:
         int n = nums.size();
         int count = 0, sum = 0;
         map<int,int> mp;
+        mp[0] = 1;
         for (int i = 0; i < n ; i++){
             sum += nums[i];
-            if (sum == k){
-                count++;
-            }
             int rem = sum - k;
             if (mp.count(rem)){
                 count += mp[rem];
