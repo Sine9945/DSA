cd5b785d3a5af7e594ede38079fa14be0859b6b4
Nirav <nirav9459@gmail.com>
2026-09-02 11:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (75.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
-        int n = s.size();
-        map<char,int> hmp1;
-        map<char,int> hmp2;
-        for (int i = 0; i < n ; i++){
-            hmp1[s[i]]++;
-            hmp2[t[i]]++;
+        array<int, 26> freq{};
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
-        if (hmp1 == hmp2) return true;
-        else return false;
+        for (int x : freq) {
+            if (x != 0) return false;
+        }
+        return true;
     }
 };
